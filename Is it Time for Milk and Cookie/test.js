6c9f07a61715e2dfc57460823c919446019bf21a
M-Mbugua <mbuguamichelle9@gmail.com>
2023-06-06 22:54:22 +0000

    Initial commit

---
+++ Is it Time for Milk and Cookie/test.js
@@ -0,0 +1,5 @@
+Test.assertEquals(timeForMilkAndCookies(new Date(2013, 11, 24)), true)
+Test.assertEquals(timeForMilkAndCookies(new Date(3000, 11, 24)), true)
+Test.assertEquals(timeForMilkAndCookies(new Date(2154, 11, 11)), false)
+Test.assertEquals(timeForMilkAndCookies(new Date(2010, 11, 2)), false)
+Test.assertEquals(timeForMilkAndCookies(new Date(1980, 9, 24)), false)
