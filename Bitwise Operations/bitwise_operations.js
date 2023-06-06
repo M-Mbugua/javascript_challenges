2df2fa9f7901e15a4218e127b632db7a1b576e46
M-Mbugua <mbuguamichelle9@gmail.com>
2023-06-06 22:04:42 +0000

    Initial commit

---
+++ Bitwise Operations/bitwise_operations.js
@@ -0,0 +1,5 @@
+const bitwiseAND = (n1, n2) => n1 & n2
+
+const bitwiseOR = (n1, n2) => n1 | n2
+
+const bitwiseXOR = (n1, n2) => n1 ^ n2
---
+++ Bitwise Operations/test.js
@@ -0,0 +1,11 @@
+Test.assertEquals(bitwiseAND(7, 12), 4)
+Test.assertEquals(bitwiseOR(7, 12), 15)
+Test.assertEquals(bitwiseXOR(7, 12), 11)
+
+Test.assertEquals(bitwiseAND(32, 17), 0)
+Test.assertEquals(bitwiseOR(32, 17), 49)
+Test.assertEquals(bitwiseXOR(32, 17), 49)
+
+Test.assertEquals(bitwiseAND(13, 19), 1)
+Test.assertEquals(bitwiseOR(13, 19), 31)
+Test.assertEquals(bitwiseXOR(13, 19), 30)
