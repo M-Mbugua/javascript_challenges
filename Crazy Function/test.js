17a7cc864023b5b60f707754159760aecdb705b2
M-Mbugua <mbuguamichelle9@gmail.com>
2023-06-07 10:28:57 +0000

    Initial commit

---
+++ Crazy Function/test.js
@@ -0,0 +1,23 @@
+// ITS EASIER THAN YOU THINK LMAO
+
+// Set 1
+Test.assertEquals(crazyfunction(10, 20), 30)
+Test.assertEquals(crazyfunction(17, 35), 50)
+Test.assertEquals(crazyfunction(61, 233), 212)
+Test.assertEquals(crazyfunction(33, 47), 14)
+Test.assertEquals(crazyfunction(18, 209), 195)
+Test.assertEquals(crazyfunction(91, 54), 109)
+Test.assertEquals(crazyfunction(88, 123), 35)
+Test.assertEquals(crazyfunction(11, 8), 3)
+
+// Set 2
+Test.assertEquals(crazyfunction(56, 80), 104)
+Test.assertEquals(crazyfunction(8, 45), 37)
+Test.assertEquals(crazyfunction(99, 201), 170)
+Test.assertEquals(crazyfunction(11, 78), 69)
+Test.assertEquals(crazyfunction(49, 139), 186)
+Test.assertEquals(crazyfunction(31, 50), 45)
+Test.assertEquals(crazyfunction(102, 113), 23)
+Test.assertEquals(crazyfunction(90, 500768), 500858)
+
+// Author : comecheckoutmycode
